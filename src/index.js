804c945ce8904d3a0f9bc4d7378a09cafb7d5e10
Claudia Mosca <mosca.claud@gmail.com>
2019-05-08 12:21:24 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Main from './components/Main/Main';
+import './components/MovieCard/MovieCard.css';
+import './components/Main/Main.css';
 
 
 ReactDOM.render(
