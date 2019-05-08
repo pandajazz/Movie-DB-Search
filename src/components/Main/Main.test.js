d6df42a26ff98f48f7e72720e66e2ffffeceac04
Claudia Mosca <mosca.claud@gmail.com>
2019-05-08 12:31:48 +0100

    Update Main.test.js

--- src/components/Main/Main.test.js
+++ src/components/Main/Main.test.js
@@ -23,29 +23,29 @@ describe('Main Component', () => {
             }
     ];
 
-	it('should have a title', () => {
+it('should have a title', () => {
         expect(wrapper.contains('The Movie DB')).toEqual(true);
 	});
 
-	it('should have a state with a movie array', () => {
-		expect(wrapper.state().movies).toEqual([]);
+it('should have a state with a movie array', () => {
+	expect(wrapper.state().movies).toEqual([]);
 	});
 
-	it('should have a list of buttons', () => {
+it('should have a list of buttons', () => {
         expect(wrapper.find(MovieButton)).toHaveLength(4);
 	});
 
-	it('should have a list of Movies', () => {
+it('should have a list of Movies', () => {
         wrapper.setState({movies: movies});
         expect(wrapper.find(MovieCard)).toHaveLength(2);
 	});
 
-    it('should reset the state', () => {
+it('should reset the state', () => {
         mainComponent.resetState();
         expect(mainComponent.state).toEqual(mainComponent.initialState);
     });
 
-    it('should get moviePage component ', () => {
+it('should get moviePage component ', () => {
         mainComponent.getSelectedMovie({title: 'Batman', id: 1})
         wrapper.update();
         expect(wrapper.find(MoviePage)).toHaveLength(1);
